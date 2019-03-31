d0b51a3017fd1e97455373e734d9a3bb66f06be1
Joey <>
2019-03-31 01:28:26 -0700

    Push to test

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,11 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    target:'node',
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'build')
-    },
-
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ["@babel/preset-env"],
-                    plugins: ["@babel/plugin-proposal-class-properties"]
-                }
-            }
-        }]
+        path: path.resolve(__dirname, 'build'),
+        libraryTarget: 'commonjs'
     },
 
     resolve: {
