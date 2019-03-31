4530c3002b7386943bc790edc59518ea1a8343e9
Joey <>
2019-03-31 00:59:54 -0700

    Push to test

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is: ',
+        headers: {
+            'Content-Type': 'text/html'
+        },
+        body: '<h1>WE DID IT!</h1>',
     });
 };
