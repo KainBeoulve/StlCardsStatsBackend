ea18333745804e3669fe73c4de96d9a2315a30f7
Joey <>
2019-03-31 01:04:54 -0700

    test

--- index.js
+++
@@ -1,9 +0,0 @@
-exports.handler = (event, context, callback) => {
-    callback(null, {
-        statusCode: '200',
-        headers: {
-            'Content-Type': 'text/html'
-        },
-        body: '<h1>WE DID IT!</h1>',
-    });
-};
