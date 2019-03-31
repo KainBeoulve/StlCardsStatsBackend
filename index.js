88561128c33225f6f55c9d36cb5deea8ce401a51
Joey <>
2019-03-31 00:44:49 -0700

    test

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.handler = (event, context, callback) => {
+    const response = {
+        statusCode: '200',
+        headers: {},
+        body: "We did it!!!!"
+    };
+    callback(null, response);
+};
