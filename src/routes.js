6a890a7b2a35e483ea0046a03a10aedb367b9d10
Joey <17524470+KainBeoulve@users.noreply.github.com>
2019-04-15 01:29:24 -0700

    Retry due to permissions issues

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,5 @@ import express from 'express';
 export const routes = express.Router();
 
 routes.get('/', (req, res) => {
-    res.status(200).set('Content-Type', 'text/html').send('<h1>WE DID IT!</h1>');
+    res.status(200).set('Content-Type', 'text/html').send('<h1>WE DID IT!!</h1>');
 });
