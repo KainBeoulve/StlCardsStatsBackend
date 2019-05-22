decd058add921bdb0815e47f713919a3ec80eeb5
Joey <17524470+KainBeoulve@users.noreply.github.com>
2019-05-22 00:02:37 -0700

    Revert "Adding options method return to backend to try"
    This reverts commit a705eb67b84f84b67b23c8786e1a306ddb9b1330.

--- src/index.js
+++ src/index.js
@@ -7,13 +7,6 @@ const Router = require("./routers/Router");
 exports.handler = (event, context) => {
     const app = express();
 
-    app.options("/*", (req, res) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-        res.send(200);
-    });
-
     app.use(bodyParser.json());
 
     app.use(cors());
